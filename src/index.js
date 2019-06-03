901b03fea203219685f50da20f55a9f166ca5ae4
salmaan72 <salmaanshaik72@gmail.com>
2019-06-03 16:08:02 +0530

    added esm transpiler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import express from 'express';
-import {router} from './routes';
-const app = express();
+// import express from 'express';
+// import {router} from './routes';
+// const app = express();
 
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const PORT = 3000;
+// const PORT = 3000;
 
-app.use('/api', router);
+// app.use('/api', router);
 
-app.listen(PORT, function() {
-    console.log(`Server listening on port ${PORT}`);
-});
+// app.listen(PORT, function() {
+//     console.log(`Server listening on port ${PORT}`);
+// });
+
+require = require('esm')(module);
+module.exports = require('./bin/dictionary');
